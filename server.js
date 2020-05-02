b552a85817c6c497df416de294e9a85bfb496455
Salvador Rodriguez <tenajesus@hotmail.com>
2020-05-01 23:14:53 -0500

    connect to MongoDB with the mongoose.connect() method.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const moongose = require('mongoose');
 
+// mongoose conection
+moongose.connect('mongodb://localhost/docentesdb', { useNewUrlParser: true, useUnifiedTopology: true });
+// express server
 const app = express();
 
-app.listen(8080,function(){
+app.listen(8080, function () {
     console.log('Servidor Iniciado')
 });
