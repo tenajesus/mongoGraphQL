1c33f7ee02a94c240380ec3d9083c00cd8b46832
Salvador Rodriguez <tenajesus@hotmail.com>
2020-05-02 22:03:07 -0500

    Export TypeDefinitions from docentes.def.js

---
+++ definitions/docentes.defs.js
@@ -0,0 +1,28 @@
+module.exports = `
+type Docentes{
+    id: ID!
+    nombre: String!
+    antiguedad : Int
+    tipo: String!
+}
+
+input DocentesInput{
+   nombre: String!
+   antiguedad:Int
+   tipo:String!
+}
+type Alerta{
+    result:String
+}
+
+type Query{
+    getDocentes(page: Int, limit: Int = 2): [Docentes]
+    getDocente(id:ID!): Docentes
+}
+
+type Mutation {
+    addDocente(input: DocentesInput) : Docentes
+    updateDocente(id:ID!,nombre:String!,antiguedad:Int,tipo:String!) : Docentes
+    deleteDocente(id:ID!): Alerta
+}
+`;
